31ecba7e8f9412f8eaca4e0c8eb9d8fc4da9a776
chenquan <chenquan.dev@gmail.com>
2022-08-06 13:13:24 +0800

    refactor: refactor code

--- internal/sys_darwin.go
+++ internal/sys_darwin.go
@@ -2,20 +2,6 @@
 
 package internal
 
-import (
-	"io/fs"
-	"syscall"
-	"time"
-)
-
-func getFileTimeInfo(fi fs.FileInfo) fileTimeInfo {
-	statT := fi.Sys().(*syscall.Stat_t)
-	return fileTimeInfo{
-		createTime: time.Unix(statT.Ctimespec.Unix()),
-		modifyTime: time.Unix(statT.Mtimespec.Unix()),
-	}
-}
-
 func accessDeniedSyscall(err error) bool {
 	return false
 }
