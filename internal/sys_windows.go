09e9e15bb8c50aba3890bb6e1e9986b98f0e2524
chen quan <chenquan.dev@gmail.com>
2023-01-10 08:15:13 +0800

    chore: remove redundant code (#65)

--- internal/sys_windows.go
+++ internal/sys_windows.go
@@ -16,14 +16,6 @@
 
 package internal
 
-import (
-	"syscall"
-)
-
-func accessDeniedSyscall(err error) bool {
-	return syscall.ERROR_ACCESS_DENIED == err
-}
-
 func sysFilter(_ string) bool {
 	return true
 }
