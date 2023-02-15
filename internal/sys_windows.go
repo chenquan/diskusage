bef1ce544acfac0c0dc68d8f32168404eb9e29ae
chenquan <chenquan.dev@gmail.com>
2023-02-15 22:18:28 +0800

    chore: upgrade the copyright

--- internal/sys_windows.go
+++ internal/sys_windows.go
@@ -1,6 +1,6 @@
 //go:build windows
 
-//   Copyright 2022 chenquan
+//   Copyright 2023 chenquan
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
