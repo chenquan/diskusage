4d00746336f861b15ad0efec2f906d4640fe01d3
chen quan <chenquan.dev@gmail.com>
2024-05-26 17:04:15 +0800

    chore: upgrade version

--- cmd/root.go
+++ cmd/root.go
@@ -25,7 +25,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const BuildVersion = "1.2.7"
+const BuildVersion = "1.2.8"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
